95fee4e91b1ea73ec4bae1e2c73037db62d272ed
xvw <xavier.vdw@gmail.com>
2017-02-27 02:43:39 +0100

    Ticket preparation

--- slider.js
+++ slider.js
@@ -1,8 +1,28 @@
 var slides = document.querySelectorAll('.cover-img');
 var length = slides.length;
+var dateb  = document.getElementById('countdown');
+var due    = new Date(dateb.getAttribute('data-due')).getTime();
+
+function formatDate(date) {
+    m  = date.getMonth();
+    d  = date.getDay();
+    h  = date.getHours();
+    mi = date.getMinutes();
+    s  = date.getSeconds();
+    v  = "" + m + " mois, ";
+    v += d + " jours, ";
+    v += h + " heures, ";
+    v += mi + " minutes et " + s + ' secondes !'
+    return v;
+
+}
+
 
 (function() {
-    var iterator = 0;
+
+    var iterator  = 0;
+    var pulsation = 0;
+
     window.setInterval(function() {
 	iterator %= length;
 	for (var i = 0; i < length; i++) {
@@ -11,4 +31,17 @@ var length = slides.length;
 	}
 	iterator += 1;
     }, 5000);
+
+
+    window.setInterval(function() {
+	pulsation %= 2;
+	var f = (pulsation == 0) ? 1.0 : 0.8;
+	var now  = new Date().getTime();
+	var count= new Date(due - now);
+	dateb.innerHTML = formatDate(count);
+	dateb.style.opacity = f;
+	pulsation += 1;
+    }, 1000);
+
+
 })();
