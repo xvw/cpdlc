936b4ee0010437ea72794d88944a11c8420ddb4b
xvw <xavier.vdw@gmail.com>
2017-05-11 07:08:41 +0200

    poushe

--- front.js
+++ front.js
@@ -1,35 +1,68 @@
-var slides = document.querySelectorAll('.cover-img');
-var length = slides.length;
-var dateb  = document.getElementById('countdown');
-var due    = new Date(dateb.getAttribute('data-due')).getTime();
+//var slides = $('.cover-img');
+//var length = slides.length;
 
-function formatDate(date) {
-    m  = date.getMonth();
-    d  = date.getDay();
-    h  = date.getHours();
-    mi = date.getMinutes();
-    s  = date.getSeconds();
-    v  = "" + m + " mois, ";
-    v += d + " jours, ";
-    v += h + " heures, ";
-    v += mi + " minutes et " + s + ' secondes !'
-    return v;
+
+function countdown() {
+  var obj = $('#countdown')
+  var dt = new Date(obj.attr('data-due')).getTime();
+  window.setInterval(function() {
+    var now = (new Date()).getTime()
+    var distance = dt - now; 
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var x = "" + days + ", " + hours + ":" + minutes + ":" + seconds
+    console.log(x)
+    obj.text(x)
+  }, 1000)
+}
+
+function scrollTo(speed) {
+  $("#down_arrow").on('click', function() {
+    var page = $(this).attr('href');
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+			return false;
+  })
+}
+
+function slider() {
+
+  var slides = $(".cover-img")
+  var length = slides.length
+  var iterator = 0;
+
+  window.setInterval(function () {
+    iterator %= length;
+    for (var i = 0; i < length; i++) {
+      var f = (iterator == i) ? 1.0 : 0.0;
+      slides[i].style.opacity = f;
+    }
+    iterator += 1;
+  }, 5000);
 
 }
 
 
-(function() {
+$(document).ready(function() { 
+  scrollTo(1024);
+  slider();
+  countdown();
+})
 
-    var iterator  = 0;
-    var pulsation = 0;
+/*
+(function () {
 
-    window.setInterval(function() {
-	iterator %= length;
-	for (var i = 0; i < length; i++) {
-	    var f = (iterator == i) ? 1.0 : 0.0;
-	    slides[i].style.opacity = f;
-	}
-	iterator += 1;
+    var iterator = 0;
+    //var pulsation = 0;
+
+    window.setInterval(function () {
+        iterator %= length;
+        for (var i = 0; i < length; i++) {
+            var f = (iterator == i) ? 1.0 : 0.0;
+            slides[i].style.opacity = f;
+        }
+        iterator += 1;
     }, 5000);
 
 
@@ -41,7 +74,7 @@ function formatDate(date) {
 	dateb.innerHTML = formatDate(count);
 	dateb.style.opacity = f;
 	pulsation += 1;
-    }, 1000);
+    }, 1000); 
 
 
-})();
+})();*/
